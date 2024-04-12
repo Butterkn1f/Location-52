9d126d03588c76f5f0ac0e535ebcd82aaf2ca8f0
Timothy Luk <154725175+timothyorsmth@users.noreply.github.com>
2024-04-12 23:26:14 +0800

    Fixed some stuff

--- Assets/Scripts/MainGame/MainGameManager.cs
+++ Assets/Scripts/MainGame/MainGameManager.cs
@@ -1,11 +1,15 @@
+using Common.DataManagement;
+using Common.DesignPatterns;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace MainGame
 {
-    public class MainGameManager : MonoBehaviour
+    public class MainGameManager : SingletonPersistent<MainGameManager>
     {
+        [SerializeField] private SaveableDataContainer GameProgressData;
+
         // Start is called before the first frame update
         void Start()
         {
@@ -17,5 +21,14 @@ namespace MainGame
         {
 
         }
+    }
+
+    public enum GameState
+    {
+        ROOM_SCENE,
+        TOWN_WALK,
+        MAIN_GAMEPLAY,
+        PHOTO_SCORING,
+        FINAL_RESULT,
     }
 }
